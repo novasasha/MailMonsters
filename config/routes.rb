7d1f091fc9304280d55033e9eff068bb6bd835f9
Jacob Golding <jcbgldng@gmail.com>
2016-09-04 12:10:12 -0500

    set up the routes the app has to take to login in with google

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'monsters#index'
+	
+	get 'auth/:provider/callback', to: 'sessions#create'
+	get 'auth/failure', to: redirect('/')
+	get 'signout', to: 'sessions#destroy', as: 'signout'
+
+	resources :sessions, only: [:create, :destroy]
+  	resource :monsters, only: [:index]
+  	root 'monsters#index'
 end
