a14820fbee90e9d020c71a52c7fdc38349ee96e8
Jacob Golding <jcbgldng@gmail.com>
2016-09-04 16:06:06 -0500

    set up the page for view a specific email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,44 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  
+  helper_method :current_user, :user_gmail, :user_gmail_inbox, :inbox_emails, :sender_name, :sender_email_address, :email_subject, :email_recieved_date, :email_text_body
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_gmail
+  	user = current_user
+	gmail = Gmail.connect(:xoauth2, user.email, user.oauth_token)
+  end
+
+  def user_gmail_inbox
+  	user_gmail.inbox
+  end
+
+  def inbox_emails
+  	user_gmail_inbox.emails
+  end
+
+  def sender_name(email)
+	name = email.sender.collect(&:name)
+	name
+  end
+
+  def sender_email_address(email)
+  	email_address = email.message.from[0]
+  	email_address
+  end
+
+  def email_subject(email)
+  	email.message.subject
+  end
+
+  def email_recieved_date(email)
+  	email.message.date
+  end
+
+  def email_text_body(email)
+  	email.message.text_part.body.decoded
   end
 end
