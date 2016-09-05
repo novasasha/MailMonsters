ad2deed1a76203df5fd04003e542d608ec8a39ca
Jacob Golding <jcbgldng@gmail.com>
2016-09-04 21:31:13 -0500

    ajax the link to single emails

--- app/assets/javascripts/document.js
+++ app/assets/javascripts/document.js
@@ -1,3 +1,27 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    $('#app-area').on('click', '.email-information', function (event) {
+    	event.preventDefault();
+    	var url = $(this).data().link
+    	viewEmail(url)
+    	
+    })
 });
+
+function fetchEmail(url) {
+	var requestEmailPromise = $.ajax({url:url, method:"GET"});
+	return requestEmailPromise
+}
+
+function viewEmail(url) {
+	var emailAjaxPromise = fetchEmail(url);
+
+
+	emailAjaxPromise.done(function(emailData) {
+		showEmail(emailData)
+	})
+	
+}
+
+function showEmail(emailData) {
+	$('#email-area').replaceWith(emailData)
+}
