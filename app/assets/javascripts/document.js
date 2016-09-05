96b7ba645d0b22626d2ca37e57d3d84521515d0b
Jacob Golding <jcbgldng@gmail.com>
2016-09-05 10:00:12 -0500

    decieded against ajax the route to opening an email

--- app/assets/javascripts/document.js
+++ app/assets/javascripts/document.js
@@ -1,27 +1,9 @@
 $( document ).ready(function() {
-    $('#app-area').on('click', '.email-information', function (event) {
-    	event.preventDefault();
-    	var url = $(this).data().link
-    	viewEmail(url)
+
+    // $('#app-area').on('click', '.email-information', function (event) {
+    // 	event.preventDefault();
+    // 	var url = $(this).data().link
+    // 	viewEmail(url)
     	
-    })
+    // })
 });
-
-function fetchEmail(url) {
-	var requestEmailPromise = $.ajax({url:url, method:"GET"});
-	return requestEmailPromise
-}
-
-function viewEmail(url) {
-	var emailAjaxPromise = fetchEmail(url);
-
-
-	emailAjaxPromise.done(function(emailData) {
-		showEmail(emailData)
-	})
-	
-}
-
-function showEmail(emailData) {
-	$('#email-area').replaceWith(emailData)
-}
