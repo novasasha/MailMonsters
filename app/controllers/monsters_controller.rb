f142c4878645ed6cba547a4eb4f9718604011a74
Jacob Golding <jcbgldng@gmail.com>
2016-09-07 16:11:26 -0500

    renamed routes

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,5 +1,6 @@
 class MonstersController < ApplicationController
 
+# determines if the user is logged in and if the user is a new user to this site
   def index
     if current_user != nil
       if current_user.monster_id == nil
@@ -10,9 +11,11 @@ class MonstersController < ApplicationController
     end
   end
 
+# links to the page the user can pick their monster
   def monster_selector
   end
 
+# saves the monster as the users choice
   def monster_choice
     monster = params[:commit].to_i
     current_user.monster_id = monster
